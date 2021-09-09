8aeacb1d746cbb4a4a89aeed4dc0cb68fef37e00
MadalitsoPhiri <Madalitso.phiri150@gmail.com>
2021-09-09 13:50:56 +0200

    changed server backends

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 import io from "socket.io-client";
-const SOCKET_URL = 'wss://sparky-chatbot-backend.herokuapp.com/';
+const SOCKET_URL = 'wss://sparkychatbot.ddns.net/';
 
 export const socket = io(SOCKET_URL,{secure: true});
